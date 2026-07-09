5426b64a9d7005150c414c1bb60644e47ac0c30d
Pieter <oosthuizenp@me.com>
2026-07-09 16:50:31 +0200

    Initial commit from copied repo

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import FaqDeck from '@/components/FaqDeck.vue';
+import HeroDeck from '@/components/HeroDeck.vue';
+import MissionDeck from '@/components/MissionDeck.vue';
+import OfferingDeck from '@/components/OfferingDeck.vue';
+import PortfolioDeck from '@/components/PortfolioDeck.vue';
+</script>
+
+<template>
+    <HeroDeck />
+    <MissionDeck />
+    <OfferingDeck />
+    <PortfolioDeck />
+    <FaqDeck />
+</template>
